7cdb73d1ae8c75fcd773e1b87924aac84faa3072
久保田慧 <[kk17slmimac@gmail.com]>
2018-10-26 20:06:41 +0900

    Initial Commit

---
+++ PresentForYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PresentForYou
+//
+//  Created by 久保田慧 on 2018/10/26.
+//  Copyright © 2018年 KeiKubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
